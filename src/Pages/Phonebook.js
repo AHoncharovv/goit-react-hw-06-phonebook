755551f2c89a5496843e119c9fe3f387fa5bc2de
Anton Honcharov <antonhoncharovv@gmail.com>
2022-07-05 22:24:19 +0300

    add route

---
+++ src/Pages/Phonebook.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import { nanoid } from 'nanoid';
+import ContactForm from "../components/ContactForm";
+import ContactList from "../components/ContactList";
+import Filter from "../components/Filter/Filter";
+
+function Phonebook() {
+  const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')) ?? []);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    window.localStorage.setItem("contacts", JSON.stringify(contacts))
+  }, [contacts])
+
+  const handlerSubmitForm = (name, number) => {
+    const newUser = { id: nanoid(5), name, number };
+    const newUserNormalized = name.toLowerCase();
+    const matchedName = contacts.find(contact => contact.name.toLowerCase() === newUserNormalized);
+
+    matchedName
+      ? alert(`${name} is already in contacts.`)
+      : setContacts([...contacts, newUser]);
+  }
+
+  const handlerFilter = event => {
+    setFilter( event.currentTarget.value );
+  }
+
+  const handleDeleteUser = event => {
+    const deleteUserId = event.currentTarget.value;
+    setContacts(contacts.filter(contact => contact.id !== deleteUserId))
+  }
+  
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+   
+  return ( 
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        color: '#010101'
+      }}
+    >
+
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={handlerSubmitForm} />
+      <h2>Contacts</h2>
+      <Filter value={filter} filterChange={handlerFilter} />
+      <ContactList users={visibleContacts} onClick={handleDeleteUser} />
+
+    </div>
+  ); 
+};
+
+export default Phonebook;
--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,16 @@
-import { useState, useEffect } from "react";
-import { nanoid } from 'nanoid';
-import ContactForm from "./ContactForm/ContactForm";
-import ContactList from "./ContactList/ContactList";
-import Filter from "./Filter/Filter";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy, Suspense } from "react";
+const Phonebook = lazy(() => import('../Pages/Phonebook.js'));
 
 function App() {
-  const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')) ?? []);
-  const [filter, setFilter] = useState("");
-
-  useEffect(() => {
-    window.localStorage.setItem("contacts", JSON.stringify(contacts))
-  }, [contacts])
-
-  const handlerSubmitForm = (name, number) => {
-    const newUser = { id: nanoid(5), name, number };
-    const newUserNormalized = name.toLowerCase();
-    const matchedName = contacts.find(contact => contact.name.toLowerCase() === newUserNormalized);
-
-    matchedName
-      ? alert(`${name} is already in contacts.`)
-      : setContacts([...contacts, newUser]);
-  }
-
-  const handlerFilter = event => {
-    setFilter( event.currentTarget.value );
-  }
-
-  const handleDeleteUser = event => {
-    const deleteUserId = event.currentTarget.value;
-    setContacts(contacts.filter(contact => contact.id !== deleteUserId))
-  }
   
-  const normalizedFilter = filter.toLowerCase();
-  const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
-   
   return ( 
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        color: '#010101'
-      }}
-    >
-
-      <h1>Phonebook</h1>
-      <ContactForm onSubmit={handlerSubmitForm} />
-      <h2>Contacts</h2>
-      <Filter value={filter} filterChange={handlerFilter} />
-      <ContactList users={visibleContacts} onClick={handleDeleteUser} />
-
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path='/' element={<Phonebook />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+    </Suspense>
   ); 
 };
 
